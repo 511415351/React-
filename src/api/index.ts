9e72e7ef735af93a262e3eee9e6978b278be1589
DDDDecade <511415351@qq.com>
2026-03-09 15:07:03 +0800

    修改文件名

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,47 +1,47 @@
-import reuquest from "../utils/reuquest";
+import request from "../utils/reuquest";
 import type { ILoginParams, IDeptSearchParams,IDept, IUserListParams, IUser, ICreateMenuParams, IMenu, ISearchParams, IUpdateMenuParams } from "../types/api";
 export default {
     login(params: ILoginParams){
-        return reuquest.post('/users/login', params);
+        return request.post('/users/login', params);
     },
 
     getDepList(params?: IDeptSearchParams){
-        return reuquest.get<IDept[]>('/dept/list', { params });
+        return request.get<IDept[]>('/dept/list', { params });
     },
     //添加部门
     createDept(data: IDept){
-        return reuquest.post('/dept/create', data);
+        return request.post('/dept/create', data);
     },
     //编辑部门
     updateDept(data: IDept){
-        return reuquest.post('/dept/edit', data);
+        return request.post('/dept/edit', data);
     },
     //删除部门
     deleteDept(id: string){
-        return reuquest.post('/dept/delete', { id });
+        return request.post('/dept/delete', { id });
     },
     //获取用户信息
     getUserList(params?: IUserListParams){
-        return reuquest.get('/users/list', { params });
+        return request.get('/users/list', { params });
     },
     getAllUser(params?: IUser){
-        return reuquest.get<IUser[]>('/users/all/list', { params });
+        return request.get<IUser[]>('/users/all/list', { params });
     },
     //菜单模块
     //菜单list
     getMenuList(params?: ISearchParams){
-        return reuquest.get<IMenu[]>('/menu/list',  params );
+        return request.get<IMenu[]>('/menu/list',  params );
     },
     //添加菜单
     createMenu(data: ICreateMenuParams){
-        return reuquest.post('/menu/create', data);
+        return request.post('/menu/create', data);
     },
     //编辑菜单
     updateMenu(data: IUpdateMenuParams){
-        return reuquest.post('/menu/edit', data);
+        return request.post('/menu/edit', data);
     },
     //删除菜单
     deleteMenu(id: string){
-        return reuquest.post('/menu/delete', { id });
+        return request.post('/menu/delete', { id });
     },
 };
--- src/api/roleApi.ts
+++ src/api/roleApi.ts
@@ -1,19 +1,19 @@
-import reuquest from "../utils/reuquest";
+import request from "../utils/reuquest";
 import type { IRole,ICreateRoleParams,IPermission,IEditRoleParams,IRoleSearchParams,ResultData } from "../types/api";
 export default {
     getRoleList(params:IRoleSearchParams){
-        return reuquest.get<ResultData<IRole>>('/roles/list',params)
+        return request.get<ResultData<IRole>>('/roles/list',params)
     },
     deleteRole(params:{_id:string}){
-        return reuquest.post('/roles/delete',params)
+        return request.post('/roles/delete',params)
     },
     updataPermission(params:IPermission){
-        return reuquest.post('/roles/update/permission',params)
+        return request.post('/roles/update/permission',params)
     },
     createRole(params:ICreateRoleParams){
-        return reuquest.post('/roles/create',params)
+        return request.post('/roles/create',params)
     },
     editRole(params:IEditRoleParams){
-        return reuquest.post('/roles/edit',params)
+        return request.post('/roles/edit',params)
     },
 }
